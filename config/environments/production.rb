44c2d9f02b12c07af704f2bf674ee1298f0d6bf9
anoosh96 <ahmedanooshwork@gmail.com>
2019-09-13 17:12:43 +0500

    Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ config.action_mailer.delivery_method = :smtp
  host = 'siety.herokuapp.com'
  config.action_mailer.default_url_options = { host: host }
  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => 'smtp.gmail.com',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => "kaih356@gmail.com",
+    :password => "qmieddryvtftflgx",
     :domain => 'heroku.com',
     :enable_starttls_auto => true }
 
